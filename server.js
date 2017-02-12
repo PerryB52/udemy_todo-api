3c27b43cd68e2472ea05f7df76ce1aebca5c8788
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-02-12 13:43:57 +0200

    Add Post for todos

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
+//https://perry1-todo-api.herokuapp.com/todos
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todoNextId = 1;
 
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-},
-{
-    id: 2,
-    description: 'Go to market',
-    completed: false
-},
-{
-    id: 3,
-    description: 'Feed the cat',
-    completed: true
-}
-];
+var todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Todo api Root');
@@ -47,6 +37,24 @@ app.get('/todos/:id', function(req, res){
     }
 
 });
+
+
+// POST /Todos /1 more npm module needed (npm install body-parser@1.13.3 --save)
+app.post('/todos', function(req, res){
+    //currently this function receives the json inputed from postman and shows it in postman
+    var body = req.body;
+
+    //add id
+    body.id = todoNextId++;
+    //todoNextId++;
+
+    //push
+    todos.push(body);
+
+    // //console.log('description: ' + body.description);
+    res.json(body);
+});
+
 
 app.listen(PORT, function(){
     console.log('Express listening on port ' + PORT + '!');
