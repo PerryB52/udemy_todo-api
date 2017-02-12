35a684f306fc233cb1bbdf80ca1d21e3136766b2
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-02-12 20:54:04 +0200

    Refactor API with _underscore

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //https://perry1-todo-api.herokuapp.com/todos
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore'); //usualy people set underscore like this
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todoNextId = 1;
 
+var todoNextId = 1;
 var todos = [];
 
 app.use(bodyParser.json());
@@ -21,14 +22,7 @@ app.get('/todos', function(req, res){
 //GET individual todos GET /todos/:id
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10); //second argument is the base, change only if you use binary/hexa etc...
-    var matchedTodo;
-
-    //itearate over todos find the match
-    todos.forEach(function (todo){
-        if(todoId === todo.id){
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
 
     if(matchedTodo){
         res.json(matchedTodo);
@@ -42,7 +36,15 @@ app.get('/todos/:id', function(req, res){
 // POST /Todos /1 more npm module needed (npm install body-parser@1.13.3 --save)
 app.post('/todos', function(req, res){
     //currently this function receives the json inputed from postman and shows it in postman
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed'); //user _.pick to only pic description and completed
+
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+
+        return res.status(404).send();
+    }
+
+    //set body.description to trimmed value
+    body.description = body.description.trim();
 
     //add id
     body.id = todoNextId++;
