cf97c67d3b2cba4cd3c5d1026f42fee996cdab2c
Alexandru Pereteanu <alex2pereteanu@gmail.com>
2017-02-24 20:14:11 +0200

    add postgrest for production

--- db.js
+++ db.js
@@ -1,9 +1,23 @@
 //server.js will request connection from db.js - db will return it and server will use
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+
+var sequelize;
+
+//proces = global object that provides info over the current node.js process - has stored multiple other objects and constants etc...
+// sequelize will be set differently depending on what environment i am running (local vs heroku)
+if (env === 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgress'
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
+
+
 
 var db = {};
 
